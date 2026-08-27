f8e84d08198335ee3b73999f516aa71c7f6d4d4f
bbzone10 <bachirbarry010@gmail.com>
2026-08-27 15:59:21 +0200

    Update bot activity name to '24/7 dnd always active'

--- src/config/bot.js
+++ src/config/bot.js
@@ -23,7 +23,7 @@ export const botConfig = {
     // 5 = Competing
     activities: [
       {
-        name: "Custom Status", // required by Discord API, not shown in the client
+        name: "24/7 dnd always active", // required by Discord API, not shown in the client
         state: "stalking",     // this is what people actually see
         type: 4,               // Custom
       },
