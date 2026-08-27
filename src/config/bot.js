6dcf4dc5ed691fd71fce615f7817690f06f3ce22
bbzone10 <bachirbarry010@gmail.com>
2026-08-27 16:03:06 +0200

    Update bot activity and primary color

--- src/config/bot.js
+++ src/config/bot.js
@@ -23,8 +23,8 @@ export const botConfig = {
     // 5 = Competing
     activities: [
       {
-        name: "24/7 dnd always active", // required by Discord API, not shown in the client
-        state: "stalking",     // this is what people actually see
+        name: ".", // required by Discord API, not shown in the client
+        state: "Not Active",     // this is what people actually see
         type: 4,               // Custom
       },
     ],
@@ -93,7 +93,7 @@ export const botConfig = {
   embeds: {
     colors: {
       // Main brand colors.
-      primary: "#336699",
+      primary: "#5302A0",
       secondary: "#2F3136",
 
       // Standard status colors for success/error/warning/info messages.
